bc347e68e28f649b8059455ae4f2552d522f66fb
godgnb <54875751+godgnb@users.noreply.github.com>
2019-11-21 15:56:58 +0900

    파씽테스트 (#3)
    * 버그 수정
    
    * 서버
    
    * 초기 커밋요청

--- src/main/java/it/company/noname/service/movieService.java
+++ src/main/java/it/company/noname/service/movieService.java
@@ -1,5 +1,15 @@
 package it.company.noname.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import it.company.noname.domain.MovieRequestVO;
+import it.company.noname.domain.MovieVO;
+
+@Service
 public interface movieService {
 
-}
+    public List<MovieVO> searchMovie(MovieRequestVO movieRequestVO); // searchMovie
+    
+} // movieService interface
