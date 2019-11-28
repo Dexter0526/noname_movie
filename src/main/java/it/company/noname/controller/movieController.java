49eccd7177062183145f4407c0388d56bf674bf8
YJ <xenexes@naver.com>
2019-11-28 15:24:12 +0900

    11-28 풀리퀘 시도
    버그 수정버전으로 수정후 다시 올림

--- src/main/java/it/company/noname/controller/movieController.java
+++ src/main/java/it/company/noname/controller/movieController.java
@@ -1,13 +1,69 @@
 package it.company.noname.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.company.noname.domain.MovieCommentVO;
+import it.company.noname.domain.MovieRequestVO;
+import it.company.noname.domain.MovieVO;
+import it.company.noname.service.movieService;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping(value="/movie/*")
+@AllArgsConstructor
 public class movieController {
+	
+	private movieService service;
+	
+	@GetMapping("/all")
+	public String searchMovie(MovieRequestVO movieRequestVO, Model model, String query) {
+		
+		// 뷰에서 사용할 정보
+		model.addAttribute("movieList", service.searchMovie(movieRequestVO));
+		model.addAttribute("query", query);
+		
+		return "movie/movieList";
+	} // searchMovie get
+	
+	
+	@GetMapping("/content")
+	public String content(MovieVO movieVO, Model model) {
+		
+		// 뷰에서 사용할 정보
+		model.addAttribute("movie", movieVO);
+		
+		return "movie/content";
+	} // content get
+	
+	
+	@PostMapping("/commentWrite")
+	public String commentWrite(String comment, MovieCommentVO movieCommentVO) {
+		
+		movieCommentVO.setEmail("aaa@naver.com");
+		movieCommentVO.setMovieCode("446212");
+		movieCommentVO.setContent(comment);
+		movieCommentVO.setLike("0");
+		movieCommentVO.setUserRating("10");
+		
+		log.info("movieCommentVO : " + movieCommentVO);
 
-}
+		
+		return "redirect:/content";
+	} // commentWrite post
+	
+	
+	
+	
+} // movieController class
---
+++ src/main/webapp/WEB-INF/views/movie/movieList.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/movie/all">
+		<input id="query" name="query" value="${query}" placeholder="제목*">
+		<input type="submit" value="영화검색">
+	</form>
+	
+	<c:forEach var="movielist" items="${movieList}">
+		<div id="movie-info">
+			이미지: <img id="image" src="${movielist.image}"><br>
+			제목: <input id="title" value="${movielist.title}">
+			링크: <input id="link" value="${movielist.link}">
+			영문제목: <input id="subtitle" value="${movielist.subtitle}">
+			개봉일자: <input id="pubDate" value="${movielist.pubDate}">
+			감독: <input id="director" value="${movielist.director}">
+			출연배우: <input id="actor" value="${movielist.actor}">
+			평점: <input id="userRating" value="${movielist.userRating}"><br>
+		</div>
+	</c:forEach>
+</body>
+</html>
