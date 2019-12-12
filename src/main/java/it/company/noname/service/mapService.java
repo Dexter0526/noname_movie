dcfe953aab142fdaf47f2e27e2594ef57f4af8a9
black4807 <56420084+black4807@users.noreply.github.com>
2019-12-12 09:11:14 +0900

    Master (#15)
    영화관 지도 api

--- src/main/java/it/company/noname/service/mapService.java
+++ src/main/java/it/company/noname/service/mapService.java
@@ -1,5 +1,13 @@
 package it.company.noname.service;
 
+import java.util.List;
+
+import it.company.noname.domain.MapVO;
+
 public interface mapService {
 
+	public List<MapVO> searchMap(String query,Integer page);
+		
+
+	
 }
